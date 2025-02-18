5f336f8f9953429be09b9664ce20edb471ff8f62
AndriiSamaiev <andriisamaiev@gmail.com>
2025-02-18 15:36:43 +0200

    modules installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
+  experimental: {
+    appManifest: false,
+  },
+  runtimeConfig: {
+    public: {
+      URI_API: process.env.VUE_APP_WEATHER_BASE_URL,
+      WEATHER_API_KEY: process.env.VUE_APP_WEATHER_API_KEY,
+    }
+  }
 })
