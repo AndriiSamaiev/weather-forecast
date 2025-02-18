c4a0c90b0cf2eae8f4c5a94f9ef9ba19534bf1c4
AndriiSamaiev <andriisamaiev@gmail.com>
2025-02-18 16:21:34 +0200

    template footer and header

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>header</div>
+  <header>
+    <nav class="flex justify-center space-x-4">
+      <NuxtLink href="#" class="font-bold rounded-lg px-3 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+        Home
+      </NuxtLink>
+      <NuxtLink href="#" class="font-bold rounded-lg px-3 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+        Team
+      </NuxtLink>
+      <NuxtLink href="#" class="font-bold rounded-lg px-3 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+        News
+      </NuxtLink>
+      <NuxtLink href="#" class="font-bold rounded-lg px-3 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+        Contacts
+      </NuxtLink>
+    </nav>
+  </header>
 </template>
 <script setup lang="ts">
 </script>
